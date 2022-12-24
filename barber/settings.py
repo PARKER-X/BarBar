797405b26f4ee401dd5bc8d7e23feb6581b9c4d9
Harsh <harshrawat096@gmail.com>
2022-12-24 17:11:50 +0530

    co

--- barber/settings.py
+++ barber/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=&2k1@i(z@$p!%-qmm4i4z&sk7c-p9$++vh3r1hufn!@+hg-s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
